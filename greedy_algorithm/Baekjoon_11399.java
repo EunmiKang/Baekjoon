cf4bac8942ad6c271ddf59c88a339ba20fe26cb6
EunmiKang <hv19962000@gmail.com>
2018-07-01 01:12:00 +0900

    Backjoon_11399_java

---
+++ greedy_algorithm/Baekjoon_11399.java
@@ -0,0 +1,28 @@
+package greedy_algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baekjoon_11399 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int result = 0;
+		
+		int N = sc.nextInt();	// 사람 수
+		int[] P = new int[N];	// 각 사람이 돈을 인출하는데 걸리는 시간을 담을 배열
+		for(int i=0; i<N; i++) {
+			P[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		Arrays.sort(P);
+		
+		for(int i=0; i<N; i++) {
+			result += (N-i) * P[i];
+		}
+
+		System.out.println(result);
+	}
+
+}
