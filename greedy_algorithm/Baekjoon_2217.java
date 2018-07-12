ae193465f0efc585f98a063f4e47429559672152
EunmiKang <hv19962000@gmail.com>
2018-07-12 22:54:46 +0900

    Baekjoon_2217_java

---
+++ greedy_algorithm/Baekjoon_2217.java
@@ -0,0 +1,29 @@
+package greedy_algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/* ทฮวม */
+
+public class Baekjoon_2217 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] ropes = new int[N];
+		for(int i=0; i<ropes.length; i++) {
+			ropes[i] = Integer.parseInt(br.readLine());
+		}
+		br.close();
+		
+		Arrays.sort(ropes);
+		
+		int max = ropes[ropes.length-1];
+		for(int i=ropes.length-2; i>=0; i--) {
+			if(max < ropes[i]*(ropes.length-i))
+				max = ropes[i]*(ropes.length-i);
+		}
+		System.out.println(max);
+	}
+}
