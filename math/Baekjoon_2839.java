731b37c15172f357454e931ca450ba6aff4ec981
EunmiKang <hv19962000@gmail.com>
2018-07-12 22:22:18 +0900

    Modify: Baekjoon_2839_java(Scanner -> BufferedReader : faster)

--- math/Baekjoon_2839.java
+++ math/Baekjoon_2839.java
@@ -1,30 +1,31 @@
 package math;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /* 설탕 배달 */
 
 public class Baekjoon_2839 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final int N = Integer.parseInt(br.readLine());
+		br.close();
 
-		int N = sc.nextInt();
-		sc.close();
-
-		switch(N%5) {
+		switch (N % 5) {
 		case 0:
-			System.out.println(N/5);
+			System.out.println(N / 5);
 			break;
 		case 1:
 		case 3:
-			System.out.println(N/5 + 1);	// 1일 때는 ( N/5 - 1 ) + 2
+			System.out.println(N / 5 + 1); // 1일 때는 ( N/5 - 1 ) + 2
 			break;
 		case 2:
 		case 4:
-			if((N == 4) || (N == 7))
+			if ((N == 4) || (N == 7))
 				System.out.println("-1");
-			else 
-				System.out.println(N/5 + 2);	// 2일 때는 ( N/5 - 2 ) + 4 , 4일 때는 ( N/5 - 1 ) + 3
+			else
+				System.out.println(N / 5 + 2); // 2일 때는 ( N/5 - 2 ) + 4 , 4일 때는 ( N/5 - 1 ) + 3
 			break;
 		}
 	}
