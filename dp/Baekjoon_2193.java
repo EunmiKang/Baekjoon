62d0546758a1c59157ce90cd21a860f9138c1720
EunmiKang <hv19962000@gmail.com>
2018-08-10 18:45:43 +0900

    Baekjoon_2193_java 수정

--- dp/Baekjoon_2193.java
+++ dp/Baekjoon_2193.java
@@ -9,9 +9,10 @@ import java.io.InputStreamReader;
 public class Baekjoon_2193 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());	//N자리
 		br.close();
 		
+		/* 틀렸다고 뜨는데 이유를 모르겠음(정답으로 채점된 코드와 비교했을 때 출력 모두 같음)
 		long[][] D = new long[N+1][2];	// 각각 끝자리 0의 개수와 1의 개수 저장
 		D[1][1] = 1;          
 		for(int i=2; i<=N; i++) {
@@ -19,5 +20,16 @@ public class Baekjoon_2193 {
 			D[i][1] = D[i-1][0];				//끝자리 1의 개수
 		}
 		System.out.println(D[N][0] + D[N][1]);
+		*/
+		
+		long dp[] = new long[N+1]; 
+		
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for(int i=2; i<dp.length; i++) {
+			dp[i] = dp[i-2] + dp[i-1];
+		}
+		System.out.println(dp[N]);    
 	}
 }
